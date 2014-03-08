24c7cfdcebf41456375c1406cddf3aa051c9bfa5
penguinmenac3 <penguinmenac3@gmail.com>
2014-03-08 14:04:36 +0100

    Created apk for shipping.

--- app/src/main/java/de/michaelfuerst/bla/UpdateApp.java
+++ app/src/main/java/de/michaelfuerst/bla/UpdateApp.java
@@ -29,8 +29,8 @@ import android.util.Log;
  * 
  */
 public class UpdateApp extends AsyncTask<String, Void, Void> {
-	private static final String VERSION = "1.1.0.3";
-	//private static final String VERSION = "dev";
+	//private static final String VERSION = "1.1.0.3";
+	private static final String VERSION = "dev";
 	private Context context;
 
 	public void setContext(Context contextf) {
