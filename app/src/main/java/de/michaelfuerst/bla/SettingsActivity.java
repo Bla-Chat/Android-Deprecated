d28909f7425bbe8aabada76f7d7d1b38212221e3
penguinmenac3 <penguinmenac3@gmail.com>
2014-03-08 14:00:44 +0100

    Fixed bug causing settings to be blank.

--- app/src/main/java/de/michaelfuerst/bla/SettingsActivity.java
+++ app/src/main/java/de/michaelfuerst/bla/SettingsActivity.java
@@ -24,7 +24,6 @@ import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
-import android.preference.PreferenceScreen;
 import android.preference.RingtonePreference;
 import android.provider.MediaStore;
 import android.text.TextUtils;
@@ -66,7 +65,7 @@ public class SettingsActivity extends PreferenceActivity {
         setupSimplePreferencesScreen();
 
         @SuppressWarnings("deprecation")
-        Preference button = (Preference)findPreference("sync_logout");
+        Preference button = findPreference("sync_logout");
         if (button != null) {
             button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 @Override
@@ -88,7 +87,7 @@ public class SettingsActivity extends PreferenceActivity {
             });
         }
         @SuppressWarnings("deprecation")
-        Preference button2 = (Preference)findPreference("sync_profile_img");
+        Preference button2 = findPreference("sync_profile_img");
         if (button2 != null) {
             button2.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 @Override
@@ -214,8 +213,6 @@ public class SettingsActivity extends PreferenceActivity {
             return;
         }
 
-        if (getPreferenceScreen()== null) return;
-
         // In the simplified UI, fragments are not used at all and we instead
         // use the older PreferenceActivity APIs.
 
@@ -225,13 +222,17 @@ public class SettingsActivity extends PreferenceActivity {
         // Add 'notifications' preferences, and a corresponding header.
         PreferenceCategory fakeHeader = new PreferenceCategory(this);
         fakeHeader.setTitle(R.string.pref_header_notifications);
-        getPreferenceScreen().addPreference(fakeHeader);
+        if (getPreferenceScreen() != null) {
+            getPreferenceScreen().addPreference(fakeHeader);
+        }
         addPreferencesFromResource(R.xml.pref_notification);
 
         // Add 'data and sync' preferences, and a corresponding header.
         fakeHeader = new PreferenceCategory(this);
         fakeHeader.setTitle(R.string.pref_header_data_sync);
-        getPreferenceScreen().addPreference(fakeHeader);
+        if (getPreferenceScreen() != null) {
+            getPreferenceScreen().addPreference(fakeHeader);
+        }
         addPreferencesFromResource(R.xml.pref_data_sync);
 
         // Bind the summaries of EditText/List/Dialog/Ringtone preferences to
